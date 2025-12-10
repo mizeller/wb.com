51e55fc9a13850bf0c58cf59780372bd813903f3
Michel Zeller <mizeller@pm.me>
2025-12-10 18:01:19 +0100

    fix: update storefront

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Need help customizing your store? - Hello World!
+            Hello Peter!
           </Heading>
         </span>
         <a
